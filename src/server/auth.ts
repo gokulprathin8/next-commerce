672143dd694c12091a1e5eeeee6640945cdd0a3f
Gokul Prathin <gokulprathin8@gmail.com>
2024-07-25 15:57:54 -0400

    fix type issue

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth"
 import { DrizzleAdapter} from "@auth/drizzle-adapter";
+import type { Adapter } from 'next-auth/adapters';
 import { db } from "@/server/db"
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    adapter: DrizzleAdapter(db),
+    adapter: DrizzleAdapter(db) as Adapter,
     providers: [
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID!,
