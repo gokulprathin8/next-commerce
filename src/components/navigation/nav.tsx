a8236360b51112cef7b6fed35644d4292e7afb07
Gokul Prathin <gokulprathin8@gmail.com>
2024-07-25 23:17:00 -0400

    added auth & login page

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -1,5 +1,8 @@
 import {auth} from "@/server/auth";
 import {UserButton} from "@/components/navigation/user-button";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {LogIn} from "lucide-react";
 
 export default async function Nav() {
     const session = await auth();
@@ -10,9 +13,22 @@ export default async function Nav() {
             <nav>
                 <ul className="flex justify-between">
                     <li>Logo</li>
-                    <li>
-                        <UserButton user={session?.user}  expires={session ? session.expires : ''} />
-                    </li>
+                    {!session ? (
+                            <li>
+                                <Button asChild={true}>
+                                    <Link className="flex gap-2" href="/auth/login">
+                                        <LogIn size={16} />
+                                        <span>Login</span>
+                                    </Link>
+
+                                </Button>
+                            </li>
+                        ) :
+                        <li>
+                            <UserButton user={session?.user} expires={session ? session.expires : ''}/>
+                        </li>
+
+                    }
                 </ul>
             </nav>
         </header>
