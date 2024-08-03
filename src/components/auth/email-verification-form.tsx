1de772b04cea68d98575a53729e5e0376e483fba
Gokul Prathin <gokulprathin8@gmail.com>
2024-08-02 20:24:02 -0400

    completed email verification

--- src/components/auth/email-verification-form.tsx
+++ src/components/auth/email-verification-form.tsx
@@ -1,10 +1,52 @@
 "use client";
 
 import {useSearchParams} from "next/navigation";
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
+import {useCallback, useEffect, useState} from "react";
+import {newVerification} from "@/server/actions/tokens";
+import {AuthCard} from "@/components/auth/auth-card";
+import {FormSuccess} from "@/components/auth/form-success";
+import FormError from "@/components/auth/form-error";
 
 export const EmailVerificationForm = () => {
     const token = useSearchParams().get('token');
     const router = useRouter();
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const handleVerification = useCallback(() => {
+        console.log('comes here 1')
+        if (success || error) return
+        console.log('comes here 2')
+        if (!token) {
+            setError("No token found!");
+            return;
+        }
+        newVerification(token).then((data) => {
+            if (data.error) {
+                setError(data.error);
+            }
+            if (data.success) {
+                setSuccess(data.success);
+                router.push('/auth/login');
+            }
+        })
+    }, []);
+
+    useEffect(() => {handleVerification()}, []);
+
+    return (
+        <AuthCard
+            cardTitle="Verifying your account"
+            backButtonHref="/auth/login"
+            backButtonLabel="Back to login"
+            showSocials={false}
+        >
+            <p>{!success && !error ? 'Verifying email...' : null}</p>
+            <FormSuccess message={success} />
+            <FormError message={error} />
+        </AuthCard>
+    )
 
 }
